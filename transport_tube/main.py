3facd79a45614c4d754a64a233182996bdc640d3
Roseline <roseline.dataengineer@gmail.com>
2023-11-01 17:55:07 -0400

    new changes

---
+++ transport_tube/main.py
@@ -0,0 +1,43 @@
+import json
+import requests
+
+def lambda_handler(req,context):
+    result = requests.get('https://api.tfl.gov.uk/line/mode/tube/status',headers={"content-type":"application/json", "charset":"utf-8"})
+
+    print("inside")
+    since_id = None
+    timeline = json.loads(result.text)
+
+    tflLineStatus = []
+    for t in timeline:
+        # Remember the first id we encounter, which is the most recent
+        if (since_id == None) :
+            since_id = t["id"]
+
+        # Add all tflLineStatus
+        tflLineStatus.append({
+            "linename": t["id"],
+            "linestatus": t["lineStatuses"][0]["statusSeverityDescription"],
+            "timestamp" : t["created"]
+        })
+    
+    # Send JSON response back to Fivetran
+
+    if(since_id==None):
+        since_id = req.state.since_id
+    ans = {
+        # // Remember the most recent id, so our requests are incremental
+        "state": {
+            since_id: since_id
+        },
+        "schema" : {
+            "tflLineStatus" : {
+                "primary_key" : ["linename"]
+            }
+        },
+        "insert": {
+            "tflLineStatus": tflLineStatus
+        },
+        "hasMore" : False
+    }
+    return ans;
